d18fd90e53ba429738378d931380cb82ceb843b9
pfcoder <pfcoder97@gmail.com>
2022-03-13 23:14:43 +0800

    update

---
+++ mock/host.js
@@ -0,0 +1,45 @@
+const Mock = require("mockjs");
+
+const List = [];
+
+module.exports = [
+  {
+    url: "/host/list",
+    type: "get",
+    response: (config) => {
+      const {
+        /*importance,
+        type,
+        title,*/
+        page = 1,
+        limit = 20,
+        //sort,
+      } = config.query;
+
+      /*let mockList = List.filter((item) => {
+        if (importance && item.importance !== +importance) return false;
+        if (type && item.type !== type) return false;
+        if (title && item.title.indexOf(title) < 0) return false;
+        return true;
+      });
+
+      if (sort === "-id") {
+        mockList = mockList.reverse();
+      }*/
+
+      let mockList = List;
+
+      const pageList = mockList.filter(
+        (item, index) => index < limit * page && index >= limit * (page - 1)
+      );
+
+      return {
+        code: 20000,
+        data: {
+          total: mockList.length,
+          items: pageList,
+        },
+      };
+    },
+  },
+];
--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+// MOCK
+const { mockXHR } = require("../mock");
+mockXHR();
+
 Vue.config.productionTip = false;
 
 new Vue({
