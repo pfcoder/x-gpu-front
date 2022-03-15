22f4a6e2651a66d15600b9fc5a2f84b1e5bf9ce8
pfcoder <pfcoder97@gmail.com>
2022-03-15 22:45:35 +0800

    update

--- mock/host.js
+++ mock/host.js
@@ -1,6 +1,24 @@
 const Mock = require("mockjs");
 
 const List = [];
+const count = 20;
+
+for (let i = 0; i < count; i++) {
+  List.push(
+    Mock.mock({
+      id: "@increment",
+      "name|1": ["RTX 3070", "RTX 2080", "P100"],
+      gpu_number: 8,
+      "gpu_ram_size|1": [11, 8],
+      cpu_type: "Intel",
+      cpu_core_number: 4,
+      "ram_size|1": [128, 64],
+      bandwidth_up: 100,
+      bandwidth_down: 100,
+      "rent_type|1": [0b1, 0b10, 0b11, 0b100, 0b110, 0b101],
+    })
+  );
+}
 
 module.exports = [
   {
