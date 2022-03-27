52510a34b588af70a5c25e5898ae164e4fc7814c
pfcoder <pfcoder97@gmail.com>
2022-03-27 23:07:20 +0800

    fix lint

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import VueCookies from 'vue-cookies';
+import VueCookies from "vue-cookies";
 
 // MOCK
 //if (process.env.NODE_ENV === "development") {
